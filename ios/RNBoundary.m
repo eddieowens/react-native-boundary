dc020a8add3d8048a7fa248ed02f8f0d692ad8a2
Nicholas Bruce <saltair93+github@gmail.com>
2019-03-04 13:27:07 -0800

    Following apple's authorization recommendation
    https://developer.apple.com/documentation/corelocation/monitoring_the_user_s_proximity_to_geographic_regions

--- ios/RNBoundary.m
+++ ios/RNBoundary.m
@@ -18,9 +18,10 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(add:(NSDictionary*)boundary addWithResolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    CLAuthorizationStatus status = [CLLocationManager locationServicesEnabled];
+    % CLAuthorizationStatus status = [CLLocationManager locationServicesEnabled];
 
-    if (!status) {
+    % if (!status) {
+    if (CLLocationManager.authorizationStatus !== kCLAuthorizationStatusAuthorizedAlways) {
         [self.locationManager requestAlwaysAuthorization];
     }
 
