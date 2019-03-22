c1fcfdf95196e8f5b166ca114cdc9ff99fd915a0
David Branton <david.branton@candide.eu>
2019-03-22 11:38:52 +0000

    (Local) Broadcast the geofence events to the parent module

--- android/src/main/java/com/eddieowens/services/BoundaryEventJobIntentService.java
+++ android/src/main/java/com/eddieowens/services/BoundaryEventJobIntentService.java
@@ -3,8 +3,10 @@ package com.eddieowens.services;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.JobIntentService;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.eddieowens.RNBoundaryModule;
 import com.eddieowens.errors.GeofenceErrorMessages;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -12,6 +14,9 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.eddieowens.RNBoundaryModule.TAG;
 
@@ -37,28 +42,29 @@ public class BoundaryEventJobIntentService extends JobIntentService {
         switch (geofencingEvent.getGeofenceTransition()) {
             case Geofence.GEOFENCE_TRANSITION_ENTER:
                 Log.i(TAG, "Enter geofence event detected. Sending event.");
-                WritableArray writableArray = Arguments.createArray();
+                final ArrayList<String> enteredGeofences = new ArrayList<>();
                 for (Geofence geofence : geofencingEvent.getTriggeringGeofences()) {
-                    writableArray.pushString(geofence.getRequestId());
+                    enteredGeofences.add(geofence.getRequestId());
                 }
-                sendEvent(ON_ENTER, writableArray);
+                sendEvent(ON_ENTER, enteredGeofences);
                 break;
             case Geofence.GEOFENCE_TRANSITION_EXIT:
                 Log.i(TAG, "Exit geofence event detected. Sending event.");
-                WritableArray writableArray1 = Arguments.createArray();
+                final ArrayList<String> exitingGeofences = new ArrayList<>();
                 for (Geofence geofence : geofencingEvent.getTriggeringGeofences()) {
-                    writableArray1.pushString(geofence.getRequestId());
+                    exitingGeofences.add(geofence.getRequestId());
                 }
-                sendEvent(ON_EXIT, writableArray1);
+                sendEvent(ON_EXIT, exitingGeofences);
                 break;
         }
     }
 
-    private void sendEvent(String event, Object params) {
-        Log.i(TAG, "Sending events " + event);
-        ((ReactApplicationContext) this.getBaseContext())
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(event, params);
-        Log.i(TAG, "Sent events");
+    private void sendEvent(String event, ArrayList<String> params) {
+        final LocalBroadcastManager lbManager =
+                LocalBroadcastManager.getInstance(this.getApplicationContext());
+        final Intent intent = new Intent(RNBoundaryModule.GEOFENCE_DATA_TO_EMIT);
+        intent.putExtra("event", event);
+        intent.putExtra("params", params);
+        lbManager.sendBroadcast(intent);
     }
 }
