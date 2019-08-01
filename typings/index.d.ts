0b23d9cfc4cf54176d62f904f6289dbc55972891
Nick Morrell <nicholascm90@gmail.com>
2019-08-01 09:44:03 -0400

    fix typing on add function

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -15,7 +15,7 @@ export interface Boundary {
 export interface BoundaryStatic {
     on: (event: Events, callback: (boundaries: string[]) => void) => EmitterSubscription;
     off: (event: Events) => void;
-    add: (boundary: Boundary) => Promise<string>;
+    add: (boundary: Boundary | Boundary[]) => Promise<string>;
     remove: (id: string) => Promise<null>;
     removeAll: () => Promise<null>;
 }
