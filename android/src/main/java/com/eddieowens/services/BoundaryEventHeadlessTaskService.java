543926f4c09c661455de546e6dbafb94ab44e166
Albert Giménez <albert.gimenez.quesada@gmail.com>
2019-08-16 03:18:38 -0500

    Fix breaking app trying to startservice in background.

--- android/src/main/java/com/eddieowens/services/BoundaryEventHeadlessTaskService.java
+++ android/src/main/java/com/eddieowens/services/BoundaryEventHeadlessTaskService.java
@@ -1,12 +1,20 @@
 package com.eddieowens.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.eddieowens.R;
 import com.facebook.react.HeadlessJsTaskService;
-import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 
 public class BoundaryEventHeadlessTaskService extends HeadlessJsTaskService {
     @Nullable
@@ -17,5 +25,29 @@ public class BoundaryEventHeadlessTaskService extends HeadlessJsTaskService {
                 extras != null ? Arguments.fromBundle(extras) : null,
                 5000,
                 true);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            String NOTIFICATION_CHANNEL_ID = "com.woffu.app.WOFFU_CHANNEL";
+            String GROUP_KEY = "com.woffu.app.WOFFU_GROUP";
+
+            Context context = this.getApplicationContext();
+
+            // Notification for the foreground service
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                    .setSmallIcon(R.drawable.common_google_signin_btn_icon_light_normal)
+                    .setPriority(NotificationManager.IMPORTANCE_LOW)
+                    .setContentTitle("Woffu Geofencing Service")
+                    .setContentText("Woffu is not spying you, or constantly tracking your location. This is a geofence, to know when you get closer to the work location.")
+                    .setOngoing(true)
+                    .setColor(ContextCompat.getColor(context, R.color.accent_material_light))
+                    .setGroup(GROUP_KEY);
+            Notification notification = builder.build();
+            startForeground(9999999, notification);
+        }
     }
 }
